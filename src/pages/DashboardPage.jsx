465d5c7165a0a5eb3856fd866c2632d1802430d3
Bupalchow <bupalchowdary06@gmail.com>
2025-07-28 19:12:12 +0530

    ,.,.,

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -14,7 +14,7 @@ import MainLayout from '../components/templates/MainLayout';
 import { Button } from '../components/atoms';
 import { ARRAYS_ROADMAP } from '../data/arraysRoadmap';
 import { STRINGS_ROADMAP } from '../data/stringsRoadmap';
-import { HASHMAPS_SETS_ROADMAP } from '../data/hashmapsSetsRoadMap';
+import { HASHMAPS_SETS_ROADMAP } from '../data/hashmapsSetsRoadmap';
 import { STACK_ROADMAP } from '../data/stackRoadmap';
 import { QUEUE_ROADMAP } from '../data/queueRoadmap';
 import { LINKED_LISTS_ROADMAP } from '../data/linkedListsRoadmap';
@@ -23,7 +23,6 @@ import { BACKTRACKING_ROADMAP } from '../data/backtrackingRoadMap';
 import { DYNAMIC_PROGRAMMING_ROADMAP } from '../data/dynamicProgrammingRoadMap';
 import { BINARY_SEARCH_ROADMAP } from '../data/binarySearchRoadMap';
 import { useRoadmapProgress } from '../hooks/useRoadmapProgress';
-import { Roadmap } from './index';
 
 
 /**
@@ -396,9 +395,6 @@ const DashboardPage = () => {
           </div>
         </div>
 
-        <div className="mt-8">
-          <Roadmap />
-        </div>
  
       </div>
     </MainLayout>
